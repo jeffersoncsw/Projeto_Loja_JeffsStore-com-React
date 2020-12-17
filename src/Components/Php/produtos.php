a5554197155b4d08cb804169e10b3a0cc14bd97c
Jefferson Wiltenburg <je_wiltenburg@hotmail.com>
2020-12-17 19:30:09 -0300

    Junção das tabelas produtos, categoria e categoria_produto concluida, BD do projeto atualizado.

--- src/Components/Php/produtos.php
+++ src/Components/Php/produtos.php
@@ -2,7 +2,14 @@
 
     require_once "db_connect.php";
 
-    $result = query("SELECT * FROM produtos");
+    $result = query(
+        "SELECT * FROM produtos
+	        inner join categoria_produto
+		        on produtos.idproduto = categoria_produto.idproduto
+	        inner join categoria
+		        on categoria_produto.idcategoria = categoria.idcategoria;
+    ");
+    
     $produtos = [];
 
     while($row = mysqli_fetch_assoc($result)){
